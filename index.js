ac93aa536499dbfd7aedf330275122e3192d1290
Tarachineno <family.takei@outlook.jp>
2025-05-30 13:17:06 +0000

    express

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello, つねぇぇぇ〜！");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, つねぇぇぇ〜！！（from Express♡）');
+});
+
+app.listen(3000, () => {
+  console.log('つねのサーバーが立ち上がりました… on port 3000');
+});
